babe0d9938487b215129fd2c007dc1b2c2ee8187
OM Miles <olivia.m.miles@gmail.com>
2021-04-24 21:32:29 -0700

    tidied up

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* Your Code Here */
-
 const createEmployeeRecord = (employee) => {
     const recordObject = {
         firstName: employee[0],
@@ -54,15 +52,6 @@ function wagesEarnedOnDate(dateStamp) {
     return payout * this.payPerHour
 }
 
-/*
-We're giving you this function. Take a look at it, you might see some usage
-that's new and different. That's because we're avoiding a well-known, but
-sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
-for you to use if you need it!
- */
-
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -74,21 +63,6 @@ let allWagesFor = function () {
 
     return payable
 }
-
-// const allWagesFor = (employeeRecordObj) => {
-//     let allDatesWorked = []
-
-//     employeeRecordObj.timeInEvents.forEach(dayWorked => {
-//         allDatesWorked.push(dayWorked.date)
-//     })
-
-//     let total = 0
-
-//     for (const dateStamp of allDatesWorked) {
-//         total += wagesEarnedOnDate(employeeRecordObj, dateStamp)
-//     }
-//     return total
-// }
 
 const findEmployeeByFirstName = (arrOfEmployeeObject, firstName) => {
     return arrOfEmployeeObject.find(employee => firstName === employee.firstName)
